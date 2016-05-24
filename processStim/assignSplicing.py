20ec31a9aa1e1cae64aa1f9181d20b3b9b5c1084
bastienboutonnet <bastien.b1@gmail.com>
2016-05-24 16:36:52 +0200

    splice assignment -done

---
+++ processStim/assignSplicing.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+def assignSplicingRules(df):
+    split=np.array_split(df,2)
+    for curDf in xrange(len(split)):
+        if curDf==0:
+            split[curDf].loc[split[curDf].rel=='related',['keepCont','splice']]=1,0
+            split[curDf].loc[split[curDf].rel=='unrelated',['keepCont','splice']]=0,1
+        else:
+            split[curDf].loc[split[curDf].rel=='related',['keepCont','splice']]=0,1
+            split[curDf].loc[split[curDf].rel=='unrelated',['keepCont','splice']]=1,0
+
+
+    concated=pd.concat(split)
+    return concated
+
+if __name__=='__main__':
+    import easygui
+    import time
+    t=time.strftime("%m%d%H%M")
+    filename=easygui.fileopenbox(filetypes=['*.csv'])
+    df=pd.read_csv(filename)
+    spliceAssignments=assignSplicingRules(df)
+    spliceAssignments.to_csv('siplceAssigned'+t+'.csv',index=False)
